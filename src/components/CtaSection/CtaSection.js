e545de8b1b79e59849e0950cf1693d4ff6b2a493
Balaji Venkatraman <rslbalaji@gmail.com>
2022-09-11 14:55:00 +0530

    Update cta section

--- src/components/CtaSection/CtaSection.js
+++ src/components/CtaSection/CtaSection.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CtaSection = (props) => {
-    return(
-        <div className="wpo-cta-section">
-            <div className="conatiner-fluid">
-                <div className="wpo-cta-item">
-                    <h2>Lets Celebrate Your Love</h2>
-                    <Link to="/home" className="theme-btn-s2">Join With Us</Link>
-                </div>
-            </div>
+  return (
+    <div className="wpo-cta-section">
+      <div className="conatiner-fluid">
+        <div className="wpo-cta-item">
+          <h2>Lets Celebrate our Love</h2>
+          <Link to="/home" className="theme-btn-s2">
+            Watch us Live
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default CtaSection;

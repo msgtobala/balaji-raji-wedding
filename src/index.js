c0b47ddbce1f512e5ebea0701c0890d58607f28e
Balaji Venkatraman <balaji@moonraft.com>
2022-12-30 01:30:12 +0530

    add analytics

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import './css/font-awesome.min.css';
 import './css/themify-icons.css';
 import './css/flaticon.css';
 import './sass/style.scss';
+import { inject } from '@vercel/analytics';
 
+inject();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 reportWebVitals();

9c6206cbb11f6eab7b2a72be16d5a39580cec227
Balaji Venkatraman <balaji@moonraft.com>
2023-01-02 16:12:10 +0530

    add image

--- src/main-component/ComingSoonPage/ComingSoonPage.js
+++ src/main-component/ComingSoonPage/ComingSoonPage.js
@@ -6,6 +6,49 @@ import shape2 from '../../images/slider/flower2.png';
 import './ComingSoonPage.css';
 
 const ComingSoonPage = () => {
+  // return (
+  //   <Fragment>
+  //     <section className="static-hero-s4" style={{ height: '100vh' }}>
+  //       <div className="hero-container">
+  //         <div className="hero-inner">
+  //           <div className="container">
+  //             <div className="coming-soon-section">
+  //               <div className="coming-soon-wrap">
+  //                 <div className="coming-soon-item">
+  //                   <div className="coming-soon-text">
+  //                     <h2>We are Marrying Soon</h2>
+  //                     <p>
+  //                       We, Balaji and Raji are getting married soon. The Cupid
+  //                       is in the process of crafting our site with the spell of
+  //                       love.
+  //                     </p>
+  //                   </div>
+  //                 </div>
+  //                 <div className="coming-soon-item">
+  //                   <div className="wpo-wedding-date change-style">
+  //                     <br />
+  //                     <h2 className="coming-soon-text">
+  //                       Time Until our Marriage
+  //                     </h2>
+  //                     <div className="clock-grids">
+  //                       <TimeCountDown date="01/26/2023"/>
+  //                     </div>
+  //                   </div>
+  //                 </div>
+  //                 <div className="shape-1">
+  //                   <img src={shape1} alt="" />
+  //                 </div>
+  //                 <div className="shape-2">
+  //                   <img src={shape2} alt="" />
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </section>
+  //   </Fragment>
+  // );
   return (
     <Fragment>
       <section className="static-hero-s4" style={{ height: '100vh' }}>
@@ -16,7 +59,7 @@ const ComingSoonPage = () => {
                 <div className="coming-soon-wrap">
                   <div className="coming-soon-item">
                     <div className="coming-soon-text">
-                      <h2>We are Marrying Soon</h2>
+                      <h2>Our Site is launching soon</h2>
                       <p>
                         We, Balaji and Raji are getting married soon. The Cupid
                         is in the process of crafting our site with the spell of
@@ -28,18 +71,18 @@ const ComingSoonPage = () => {
                     <div className="wpo-wedding-date change-style">
                       <br />
                       <h2 className="coming-soon-text">
-                        Time Until our Marriage
+                        Time Until our website launch
                       </h2>
                       <div className="clock-grids">
-                        <TimeCountDown date="01/26/2023"/>
+                        <TimeCountDown date="01/03/2023" />
                       </div>
                     </div>
                   </div>
                   <div className="shape-1">
-                    <img src={shape1} alt="" />
+                    <img src={shape1} alt="shape1" />
                   </div>
                   <div className="shape-2">
-                    <img src={shape2} alt="" />
+                    <img src={shape2} alt="shape2" />
                   </div>
                 </div>
               </div>

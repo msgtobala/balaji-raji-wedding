c4fad7fc65229391bc8bbd4f320e7af4afbeb1dc
Balaji Venkatraman <balaji@moonraft.com>
2023-01-02 13:10:25 +0530

    add share logic

--- src/main-component/GamesPage/Games/ShareAndEarn/ShareAndEarn.js
+++ src/main-component/GamesPage/Games/ShareAndEarn/ShareAndEarn.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RWebShare } from 'react-web-share';
+import { doc, updateDoc, increment, onSnapshot } from 'firebase/firestore';
 
-import 'react-fancybox/lib/fancybox.css';
 import './styles.css';
 import GameNavigation from '../GameNavigation';
 import { websiteImages as Images } from '../../../../constants/images';
+import { addGrabGems } from '../../../../helpers/addGrabGems';
+import { db } from '../../../../services/firebase';
 
 const ShareAndEarn = () => {
+  const [noOfShares, setNoOfShares] = useState(0);
+  const [grabGemsEarned, setGrabGemsEarned] = useState(0);
+
+  const shareSuccess = async () => {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+
+    if (user) {
+      addGrabGems(1);
+      const query = doc(db, 'users', user.mobile);
+      await updateDoc(query, {
+        grabGemsEarnedOnShare: increment(1 ?? 0),
+        noOfShares: increment(1 ?? 0),
+      });
+    }
+  };
+
+  useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    let unsubscribe;
+    if (user) {
+      const query = doc(db, 'users', user.mobile);
+      unsubscribe = onSnapshot(query, (querySnapshot) => {
+        const data = querySnapshot.data();
+        if (data) {
+          setNoOfShares(data.noOfShares);
+          setGrabGemsEarned(data.grabGemsEarnedOnShare);
+        }
+      });
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="container">
       <div className="game-space">
@@ -15,20 +49,20 @@ const ShareAndEarn = () => {
         </div>
         <div className="share-results">
           <div>
-            Shares: <span>2</span>
+            Shares: <span>{noOfShares}</span>
           </div>
           <div>
-            Grab Gems: <span>2</span>
+            Grab Gems: <span>{grabGemsEarned}</span>
           </div>
         </div>
         <div className="share-container">
           <RWebShare
             data={{
-              text: 'Like humans, flamingos make friends for life',
+              text: '1 Share, 1 Grab Gem',
               url: 'https://balaji-raji-wedding.vercel.app/',
               title: 'Share our wedding website to earn Grab gems',
             }}
-            onClick={() => console.info('share successful!')}
+            onClick={shareSuccess}
           >
             <button>
               <i className="ti-sharethis"></i> Share
